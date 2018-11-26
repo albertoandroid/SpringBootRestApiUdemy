b3b2859b3626f294b7de02cef4248e3f2b0b7069
Alberto <info@androiddesdecero.com>
2018-11-26 16:25:44 +0100

    12InterfazService

---
+++ src/main/java/com/example/curso/service/IProfesorService.java
@@ -0,0 +1,28 @@
+package com.example.curso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.curso.entity.Profesor;
+
+public interface IProfesorService {
+	
+	public List<Profesor> findAll();
+	
+	public Profesor findProfesor(Profesor profesor);
+	
+	public Profesor checkProfesorLogin(Profesor profesor);
+	
+	public void deleteProfesro(Profesor profesor);
+	
+	public Profesor uptadeProfesor(Profesor profesor);
+	
+	public Optional<Profesor> findProfesorById(Long profesor_id);
+	
+	public void deleteProfesor(Long id);
+	
+	public Profesor findById(Long id);
+	
+	public Profesor findByIdSQL(Long id);
+	
+}
