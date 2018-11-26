470677d25ceb01caab1525cc5836e8a58a0ab589
Alberto <info@androiddesdecero.com>
2018-11-26 20:17:40 +0100

    15RestControllerSave

--- src/main/java/com/example/curso/controller/ProfesorRestController.java
+++ src/main/java/com/example/curso/controller/ProfesorRestController.java
@@ -1,5 +1,40 @@
 package com.example.curso.controller;
 
-public class ProfesorRestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.curso.entity.Profesor;
+import com.example.curso.service.IProfesorService;
+
+@RestController
+@RequestMapping("/api")
+public class ProfesorRestController {
+	
+	@Autowired
+	private IProfesorService profesorService;
+	
+	@GetMapping("/profesores")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Profesor> getProfesores(){
+		return profesorService.findAll();
+	}
+	
+	@PostMapping("/sign_up")
+	public ResponseEntity<Void> addProfesor(@RequestBody Profesor profesor){
+		if(profesorService.findProfesor(profesor)==null) {
+			profesorService.save(profesor);
+			return new ResponseEntity<Void>(HttpStatus.CREATED);
+		}else {
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+	}
 }
